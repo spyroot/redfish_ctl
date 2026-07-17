edceb45b5f1b4a5789b9d603364f1f8cda6dd5fe
spyroot@gmail.com <spyroot@gmail.com>
2026-07-18 00:12:13 +0400

    Add Dell LC network share test command

--- redfish_ctl/redfish_manager_shared.py
+++ redfish_ctl/redfish_manager_shared.py
@@ -41,6 +41,7 @@ class ApiRequestType(Enum):
 
     RemoteServicesRssAPIStatus = auto()
     RemoteServicesAPIStatus = auto()
+    DellLcNetworkShareTest = auto()
     TasksList = auto()
 
     ChangeBootOrder = auto()
